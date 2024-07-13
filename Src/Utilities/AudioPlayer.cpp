59127c14ce4bc32cb70d929dbca427a784c89ffc
Defisym <defisym@outlook.com>
2024-07-13 11:54:13 +0800

    Convert audio data to PCM

--- Src/Utilities/AudioPlayer.cpp
+++ Src/Utilities/AudioPlayer.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL.h>
 #include <SDL_mixer.h>
+#include <limits>
 
 constexpr auto SDL_BUFFER_SIZE = 4096;
 
@@ -16,8 +17,11 @@ bool AudioPlayer::InitAudio() {
 
 	if (!Mix_AudioOpened()) {
 		// If your app needs precisely what is requested, specify zero for `allowed_changes`
-		if (Mix_OpenAudioDevice(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, SDL_BUFFER_SIZE,
-								nullptr, 0) == -1) {
+		if (Mix_OpenAudioDevice(MIX_DEFAULT_FREQUENCY,
+			MIX_DEFAULT_FORMAT,
+			MIX_DEFAULT_CHANNELS,
+			SDL_BUFFER_SIZE,
+			nullptr, 0) == -1) {
 			auto error = SDL_GetError();
 
 			return false;
@@ -27,4 +31,21 @@ bool AudioPlayer::InitAudio() {
 	Mix_AllocateChannels(MIX_CHANNELS);
 
 	return true;
+}
+
+// Convert data for AUDIO_S16SYS, signed int16_t
+// Source is single channel
+void AudioPlayer::ConvertData(const SourceInfo& srcInfo, const DestInfo& destInfo) {
+	constexpr auto maxValue = std::numeric_limits<int16_t>::max();
+	const auto sampleCount = static_cast<uint16_t>(static_cast<double>(srcInfo.duration) / 1000.0 * MIX_DEFAULT_FREQUENCY);
+
+	auto pBuffer = destInfo.pBuffer;
+	for (size_t index = 0; index < srcInfo.bufferSz; index++) {
+		const auto convertResult = static_cast<int16_t>(srcInfo.pBuffer[index] * maxValue);
+		const auto repeat = sampleCount * 2; // both channels are the same
+
+		for (uint16_t repIdx = 0; repIdx < repeat; repIdx++, pBuffer++) {
+			*pBuffer = convertResult;
+		}
+	}
 }
--- Src/Utilities/AudioPlayer.h
+++ Src/Utilities/AudioPlayer.h
@@ -1,10 +1,30 @@
 #pragma once
+#include "../../Src/Module/General/Definition.h"
 
 // PCM data:
-// 16 bit mono:
-//  | high | low | high | low |...
-// 16 bit stereo:
-//  | ch0 high | ch0 low | ch1 high | ch1 low |...
+//  16 bit mono:
+//      | high | low | high | low |...
+//  16 bit stereo:
+//      | ch0 high | ch0 low | ch1 high | ch1 low |...
+// PCM data range:
+//  Signed 16 bit: -32768 ~ 32767
+// Sample rate:
+//  Point per second
 struct AudioPlayer {
     inline static bool InitAudio();
+
+	struct SourceInfo {
+        // pBuffer should be normalized to -1.0f ~ 1.0f
+        OTDRProcessValueType* pBuffer;
+    	size_t bufferSz;
+        // in ms
+        size_t duration;
+    };
+
+    struct DestInfo {
+        int16_t* pBuffer;
+        size_t bufferSz;
+    };
+
+    inline static void ConvertData(const SourceInfo& srcInfo, const DestInfo& destInfo);
 };
