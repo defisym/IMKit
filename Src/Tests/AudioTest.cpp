140f5c98cd0e1ab080468c9f76f35bef0d99b3a9
Defisym <defisym@outlook.com>
2024-09-20 18:56:26 +0800

    Update test case to generate wave of given frequency

--- Src/Tests/AudioTest.cpp
+++ Src/Tests/AudioTest.cpp
@@ -14,17 +14,21 @@
 #include "./../Utilities/AudioPlayer.h"
 
 namespace AudioTest {
-    const OTDRProcessValueType* AllocBuffer(const size_t sz = 1000) {
+    constexpr auto DEFAULT_BUFFER_SZ = 16000;
+    constexpr auto DEFAULT_FREQ = 200.0;
+
+    const OTDRProcessValueType* AllocBuffer(const size_t sz = DEFAULT_BUFFER_SZ) {
 #ifdef USE_CONSOLE
         AllocConsole();
         freopen("CONIN$", "r", stdin);
         freopen("CONOUT$", "w", stdout);
         freopen("CONOUT$", "w", stderr);
 #endif
-
         const auto p = new OTDRProcessValueType[sz];
+        const auto dt= 1.0 / static_cast<double>(sz);
+
         for (size_t index = 0; index < sz; index++) {
-            p[index] = static_cast<OTDRProcessValueType>(std::sin(PI * index / 200.0));
+            p[index] = static_cast<OTDRProcessValueType>(std::sin(2 * PI * DEFAULT_FREQ * dt * static_cast<double>(index)));
 
 #ifdef USE_CONSOLE
             std::cout << p[index] << ", ";
@@ -44,7 +48,7 @@ namespace AudioTest {
         Sleep(500 + rand() % 10);
 
         for (;;) {
-            player.AddData(audioData, { p,1000,500 });
+            player.AddData(audioData, { p,DEFAULT_BUFFER_SZ,500 });
             Sleep(500 + rand() % 10);
         }
     }
@@ -52,8 +56,8 @@ namespace AudioTest {
     [[noreturn]] void AudioHandlerTest() {
         const auto p = AllocBuffer();
 
-        AudioHandler handler = { 500 };
-        handler.AddData({ p,1000,500 });
+        AudioHandler handler = { 1000 };
+        handler.AddData({ p,DEFAULT_BUFFER_SZ,1000 });
 
         //Sleep(500 + rand() % 10);
 
