f7255dcf729998c053c1711e47bb97e457399354
Defisym <defisym@outlook.com>
2024-10-17 10:29:37 +0800

    update macro comments

--- Src/macro.h
+++ Src/macro.h
@@ -4,9 +4,20 @@
 // Macro
 // ------------------------------------------------
 
-#define ALWAYS_SHOW_CONFIG
-#define ONLY_VIBRATION_LOCALIZATION_RESULT
+// ------------------------------------
+// Debug only
+// ------------------------------------
+#ifdef _DEBUG
+// always show device config even if no device created
+#define CONFIG_ALWAYS_SHOW_DEVICE_PARAM
+#endif
+
+// ------------------------------------
+// General
+// ------------------------------------
+// only show the result
+#define VIBRATION_LOCALIZATION_ONLY_SHOW_RESULT
 #define WAVEFORM_RESTORE_ONLY_SHOW_RESULT
 
 // try to read saved config even if the struct size changed
-#define TRY_READ
+#define CONFIG_TRY_READ_CONFIG
