8c62d67fe0d880828cbf1c21a8361c53f06c4963
defisym <defisym@outlook.com>
2024-10-23 18:04:41 +0800

    fix spin size

--- Src/IMGuiEx/AddSpin.h
+++ Src/IMGuiEx/AddSpin.h
@@ -9,14 +9,15 @@ template<typename T>
 inline bool AddSpin(const char* label,
     T* v, T min, T max, T step = 1) {
     auto bChanged = false;
+    const float button_size = ImGui::GetFrameHeight();
 
     ImGui::SameLine();
-    if (ImGui::Button(std::format("-##{}", label).c_str())) {
+    if (ImGui::Button(std::format("-##{}", label).c_str(), ImVec2(button_size, button_size))) {
         *v = (std::max)(*v - step, min);
         bChanged = true;
     }
     ImGui::SameLine();
-    if (ImGui::Button(std::format("+##{}", label).c_str())) {
+    if (ImGui::Button(std::format("+##{}", label).c_str(), ImVec2(button_size, button_size))) {
         *v = (std::min)(*v + step, max);
         bChanged = true;
     }
