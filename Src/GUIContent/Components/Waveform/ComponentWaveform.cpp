03057e83779a7ad7bdec457e483a120a8a705d43
Defisym <defisym@outlook.com>
2024-11-03 13:36:14 +0800

    Update ComponentWaveform.cpp

--- Src/GUIContent/Components/Waveform/ComponentWaveform.cpp
+++ Src/GUIContent/Components/Waveform/ComponentWaveform.cpp
@@ -1,11 +1,25 @@
 #include "ComponentWaveform.h"
 
 #include "Process/ComponentProcess.h"
+#include "Inspect/ComponentInspect.h"
 
 void ComponentWaveform(Ctx* pCtx) {
     if (!ImGui::CollapsingHeader(I18N("Waveform"), ImGuiTreeNodeFlags_DefaultOpen)) {
         return;
     }
+    if (ImGui::BeginTabBar("Waveform/Tab", TAB_BAR_FLAGS)) {
+        if (ImGui::BeginTabItem(I18N("Process"))) {
+            ComponentProcess(pCtx);
 
-    ComponentProcess(pCtx);
+            ImGui::EndTabItem();
+        }
+
+        if (ImGui::BeginTabItem(I18N("Inspect"))) {
+            ComponentInspect(pCtx);
+
+            ImGui::EndTabItem();
+        }
+
+        ImGui::EndTabBar();
+    }
 }
