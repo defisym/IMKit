e5c5ec8c8de861603c6ea847fbfeb23b33d8b474
Defisym <defisym@outlook.com>
2024-11-28 18:18:02 +0800

    `ThreadBase`

--- Src/Worker/ThreadBase.cpp
+++ Src/Worker/ThreadBase.cpp
@@ -1,1 +1,49 @@
 #include "ThreadBase.h"
+
+#include <format>
+
+const char* ThreadBase::GetThreadName(const char* pName) {
+    static size_t threadIndex = 0u;
+    static std::string threadName = {};
+
+    if (pName != nullptr) { return pName; }
+
+    threadName = std::format("thread_{}", threadIndex);
+    pName = threadName.c_str();
+    threadIndex++;
+
+    return pName;
+}
+
+bool ThreadBase::Start(const ThreadInfo& info) {
+    if (pThread != nullptr) { return false; }
+
+    pThread = SDL_CreateThread([] (void* pData)->int {
+                               return static_cast<ThreadBase*>(pData)->Worker();
+                               },
+                               GetThreadName(info.pName), this);
+    if (pThread == nullptr) { const auto err = SDL_GetError(); return false; }
+
+    if (info.bDetach) {
+        SDL_AtomicSet(&detachThread, ThreadConstanst::DETACH);
+        SDL_DetachThread(pThread);
+    }
+
+    return true;
+}
+
+bool ThreadBase::Stop() {
+    do {
+        if (pThread == nullptr) { break; }
+        if (SDL_AtomicGet(&detachThread) == ThreadConstanst::DETACH) { break; }
+        if (SDL_AtomicGet(&quitThread) == ThreadConstanst::QUIT) { break; }
+
+        SDL_AtomicSet(&quitThread, ThreadConstanst::WAITING);
+        SDL_WaitThread(pThread, &threadReturn);
+        SDL_AtomicSet(&quitThread, ThreadConstanst::QUIT);
+
+        return true;
+    } while (false);
+
+    return false;
+}
--- Src/Worker/ThreadBase.h
+++ Src/Worker/ThreadBase.h
@@ -1,47 +1,38 @@
 #pragma once
 
-#include <format>
-
 #include <SDL.h>
 #include <SDL_thread.h>
 #include <SDL_atomic.h>
 
-namespace ThreadState {
-    constexpr int Work = 0;
-    constexpr int Quit = 1;
+namespace ThreadConstanst {
+    constexpr int WORK = 0;
+    constexpr int WAITING = 1;
+    constexpr int QUIT = 2;
+
+    constexpr int ATTACH = 0;
+    constexpr int DETACH = 1;
 }
 
+struct ThreadInfo {
+    const char* pName = nullptr;
+    bool bDetach = false;
+};
+
 class ThreadBase {
-    SDL_atomic_t quit = { ThreadState::Work };
     SDL_Thread* pThread = nullptr;
     SDL_threadID threadId = 0;
     int threadReturn = 0;
 
+    SDL_atomic_t detachThread = { ThreadConstanst::ATTACH };
+    SDL_atomic_t quitThread = { ThreadConstanst::WORK };
+
+    // generate a name if name is nullptr
+    static const char* GetThreadName(const char* pName = nullptr);
+
 public:
-    virtual ~ThreadBase() { Stop(); }
+    virtual ~ThreadBase() = default;
     virtual int Worker() = 0;
 
-    void Start(const char* pName = nullptr) {
-        // generate a name if pName is nullptr
-        static size_t threadIndex = 0u;
-        static std::string threadName = {};
-
-        threadIndex++;
-        if (pName == nullptr) {
-            threadName = std::format("thread_{}", threadIndex);
-            pName = threadName.c_str();
-        }
-
-        pThread = SDL_CreateThread([] (void* pData)->int {
-            return static_cast<ThreadBase*>(pData)->Worker();
-        },
-        pName, this);
-    }
-    void Stop() {
-        // already triggered
-        if (SDL_AtomicGet(&quit) == ThreadState::Quit) { return; }
-
-        SDL_AtomicSet(&quit, ThreadState::Quit);
-        SDL_WaitThread(pThread, &threadReturn);
-    }
+    bool Start(const ThreadInfo& info = {});
+    bool Stop();
 };
