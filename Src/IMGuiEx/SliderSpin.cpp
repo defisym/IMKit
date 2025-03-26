3ed9a3907c65e188a193953b5d0d86199d4d3d3c
defisym <defisym@outlook.com>
2025-03-26 17:58:52 +0800

    `SliderSpinInt` step param

--- Src/IMGuiEx/SliderSpin.cpp
+++ Src/IMGuiEx/SliderSpin.cpp
@@ -3,13 +3,13 @@
 #include "AddSpin.h"
 
 bool SliderSpinInt(const char* label,
-                   int* v, int v_min, int v_max,
+                   int* v, int v_min, int v_max, int step,
                    const char* format, const ImGuiSliderFlags flags) {
     bool bUpdate = false;
 
     bUpdate |= ImGui::SliderInt(std::format("##{}", label).c_str(),
         v, v_min, v_max, format, flags);
-    bUpdate |= AddSpin(label, v, v_min, v_max);
+    bUpdate |= AddSpin(label, v, v_min, v_max, step);
 
     return bUpdate;
 }
--- Src/IMGuiEx/SliderSpin.h
+++ Src/IMGuiEx/SliderSpin.h
@@ -3,5 +3,5 @@
 #include "imgui.h"
 
 bool SliderSpinInt(const char* label,
-    int* v, int v_min, int v_max,
+    int* v, int v_min, int v_max, int step = 1,
     const char* format = "%d", ImGuiSliderFlags flags = 0);
