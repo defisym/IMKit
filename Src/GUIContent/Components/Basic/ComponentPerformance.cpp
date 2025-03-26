05f95809ea7a21add1118514b62de679ec6546f6
defisym <defisym@outlook.com>
2025-03-26 16:44:38 +0800

    update measure

--- Src/GUIContent/Components/Basic/ComponentPerformance.cpp
+++ Src/GUIContent/Components/Basic/ComponentPerformance.cpp
@@ -29,15 +29,54 @@ void ComponentPerformance(Ctx* pCtx) {
 #ifdef MULTITHREAD
     const auto helper = performaceParams.GetUILockHelper();
 #endif
+    {
+        ImGui::TextUnformatted(I18N("Current: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Read Data: %.3f ms"), performaceParams.read.cur.readTime);
+        ImGui::SameLine();
+        
+        ImGui::TextUnformatted(I18N("Average: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Read Data: %.3f ms"), performaceParams.read.avg.readTime);
+        ImGui::SameLine();
+     
+        ImGui::TextUnformatted(I18N("Slowest: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Read Data: %.3f ms"), performaceParams.read.slowest.readTime);
+    }
+    {
+        ImGui::TextUnformatted(I18N("Current: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Vibration Localization: %.3f ms, Waterfall Chat: %.3f ms, Log: %.3f ms"),
+            performaceParams.vl.cur.processTime, performaceParams.vl.cur.waterfallChatTime, performaceParams.vl.cur.logTime);
 
-    ImGui::Text(I18N("Read Data: %.3f ms"), performaceParams.readTime);
-    ImGui::SameLine();
+        ImGui::TextUnformatted(I18N("Average: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Vibration Localization: %.3f ms, Waterfall Chat: %.3f ms, Log: %.3f ms"),
+            performaceParams.vl.avg.processTime, performaceParams.vl.avg.waterfallChatTime, performaceParams.vl.avg.logTime);
+        
+        ImGui::TextUnformatted(I18N("Slowest: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Vibration Localization: %.3f ms, Waterfall Chat: %.3f ms, Log: %.3f ms"),
+            performaceParams.vl.slowest.processTime, performaceParams.vl.slowest.waterfallChatTime, performaceParams.vl.slowest.logTime);
+    }
+    {
+        ImGui::TextUnformatted(I18N("Current: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Waveform Restore: %.3f ms, Peak Restore: %.3f ms, Log UI: %.3f ms, Log: %.3f ms"),
+            performaceParams.wr.cur.processTime, performaceParams.wr.cur.peakProcess,
+            performaceParams.wr.cur.logUITime, performaceParams.wr.cur.logTime);
 
-    ImGui::Text(I18N("Vibration Localization: %.3f ms, Waterfall Chat: %.3f ms, Log: %.3f ms"),
-        performaceParams.vl.processTime, performaceParams.vl.waterfallChatTime, performaceParams.vl.logTime);
-    ImGui::SameLine();
+        ImGui::TextUnformatted(I18N("Average: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Waveform Restore: %.3f ms, Peak Restore: %.3f ms, Log UI: %.3f ms, Log: %.3f ms"),
+            performaceParams.wr.avg.processTime, performaceParams.wr.avg.peakProcess,
+            performaceParams.wr.avg.logUITime, performaceParams.wr.avg.logTime);
 
-    ImGui::Text(I18N("Waveform Restore: %.3f ms, Peak Restore: %.3f ms, Log UI: %.3f ms, Log: %.3f ms"),
-        performaceParams.wr.processTime, performaceParams.wr.peakProcess,
-        performaceParams.wr.logUITime, performaceParams.wr.logTime);
+        ImGui::TextUnformatted(I18N("Slowest: "));
+        ImGui::SameLine();
+        ImGui::Text(I18N("Waveform Restore: %.3f ms, Peak Restore: %.3f ms, Log UI: %.3f ms, Log: %.3f ms"),
+            performaceParams.wr.slowest.processTime, performaceParams.wr.slowest.peakProcess,
+            performaceParams.wr.slowest.logUITime, performaceParams.wr.slowest.logTime);
+    }
 }
