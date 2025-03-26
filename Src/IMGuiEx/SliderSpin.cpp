94fdab13062a7942b4a463fcd11c7a54a1fac5b8
defisym <defisym@outlook.com>
2025-03-26 18:14:02 +0800

    `SliderSpinFloat`

--- Src/IMGuiEx/SliderSpin.cpp
+++ Src/IMGuiEx/SliderSpin.cpp
@@ -13,3 +13,15 @@ bool SliderSpinInt(const char* label,
 
     return bUpdate;
 }
+
+bool SliderSpinFloat(const char* label,
+                     float* v, float v_min, float v_max, float step,
+                     const char* format, ImGuiSliderFlags flags) {
+    bool bUpdate = false;
+
+    bUpdate |= ImGui::SliderFloat(std::format("##{}", label).c_str(),
+        v, v_min, v_max, format, flags);
+    bUpdate |= AddSpin(label, v, v_min, v_max, step);
+
+    return bUpdate;
+}
--- Src/IMGuiEx/SliderSpin.h
+++ Src/IMGuiEx/SliderSpin.h
@@ -5,3 +5,7 @@
 bool SliderSpinInt(const char* label,
     int* v, int v_min, int v_max, int step = 1,
     const char* format = "%d", ImGuiSliderFlags flags = 0);
+
+bool SliderSpinFloat(const char* label,
+    float* v, float v_min, float v_max, float step = 0.1,
+    const char* format = "%.3f", ImGuiSliderFlags flags = 0);
