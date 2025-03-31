4db7cc19c40a7f11c178aca4d6335c21743e5acc
Defisym <defisym@outlook.com>
2025-03-31 17:00:30 +0800

    ditto

--- Src/GUIContent/Components/Basic/ComponentWindow.h
+++ Src/GUIContent/Components/Basic/ComponentWindow.h
@@ -1,22 +1,6 @@
 #pragma once
 
-#include "ComponentBase.h"
-
-struct ComponentWindow :ComponentBase {  // NOLINT(cppcoreguidelines-special-member-functions)
-	ComponentWindow(Ctx* p) :ComponentBase(p) {
-		// ref: https://stackoverflow.com/questions/74954814/remove-the-parent-default-window-in-imgui
-		// place the next window in the top left corner (0,0), resize to parent window size
-		ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
-		ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-
-		// hide it
-		ImGuiWindowFlags window_flags = 0;
-		window_flags |= ImGuiWindowFlags_NoTitleBar;
-		window_flags |= ImGuiWindowFlags_NoMove;
-		window_flags |= ImGuiWindowFlags_NoResize;
-
-		// Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-		ImGui::Begin("Window", nullptr, window_flags);
-	}
-	~ComponentWindow() { ImGui::End(); }
+struct ComponentWindow {  // NOLINT(cppcoreguidelines-special-member-functions)
+    ComponentWindow(const char* pName = "Window");
+	~ComponentWindow();
 };
