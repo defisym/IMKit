2bd5db89a5d3ae733b34ec4c74396b75b1bce210
Defisym <defisym@outlook.com>
2025-03-31 16:23:07 +0800

    `Waveform` -> `Display`

--- Src/GUIContent/Components/Waveform/Inspect/ComponentInspect.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "GUIContext/GUIContext.h"
-
-void ComponentInspect(const Ctx* pCtx);
--- Src/GUIContent/Components/Waveform/Logger/ComponentLogger.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "GUIContext/GUIContext.h"
-
-void ComponentLogger(Ctx* pCtx);
