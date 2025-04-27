7c0b880f85c01d06eb06e9779fa48f579d865631
Defisym <defisym@outlook.com>
2025-04-27 14:13:44 +0800

    remove old contents

--- Src/Utilities/Logger.cpp
+++ Src/Utilities/Logger.cpp
@@ -65,12 +65,12 @@ void Logger::AddLog(const std::string&& log) {
     lines.emplace_back(log);
 }
 
-void Logger::AddLog(LogDataInterface* pLogData) {
-    AddLog(std::format("[{}] {}: {}",
-        GetFormattedTimeStamp(),
-        pLogData->DataTypeInfo(),
-        pLogData->ToString()));
-}
+//void Logger::AddLog(LogDataInterface* pLogData) {
+//    AddLog(std::format("[{}] {}: {}",
+//        GetFormattedTimeStamp(),
+//        pLogData->DataTypeInfo(),
+//        pLogData->ToString()));
+//}
 
 void Logger::AddLog(const char* pType, const char* pLog) {
     AddLog(std::format("[{}] {}: {}",
@@ -82,156 +82,4 @@ void Logger::AddLog(const std::string& type, const std::string& log) {
     AddLog(std::format("[{}] {}: {}",
         GetFormattedTimeStamp(),
         type, log));
-}
-
-std::size_t std::hash<FileLoggerConfig>::operator()(FileLoggerConfig const& s) const noexcept {
-    std::size_t hash = 0xcbf29ce484222325; // FNV-1a
-    hash ^= std::hash<size_t>{}(s.interval);
-    hash *= 0x100000001b3;  // FNV-1a
-
-    hash ^= GetStringHash(s.filePath);
-    hash *= 0x100000001b3;  // FNV-1a
-
-    return hash;
-}
-
-FileLogger::FileLogger(const FileLoggerConfig& config) {
-    this->config = config;
-    
-    do {
-        // get relative path
-        char fullPathName[MAX_PATH] = {};
-        GetFullPathNameA(config.filePath, MAX_PATH, fullPathName, nullptr);
-        filePath = fullPathName;
-
-        // GetFullPathName will normalize / and \\ to \\
-        // if not end with \\ , append it
-        if (!filePath.ends_with('\\')) { filePath += '\\'; }
-
-        // create dir
-        const auto path = fs::path{ filePath };
-
-        std::error_code ec = {};
-        fs::create_directories(path, ec);
-
-        if (ec.value() != 0) { break; }
-
-        bValid = true;
-
-        return;
-    } while (false);
-
-    bValid = false;   
-}
-
-FileLogger::~FileLogger() { SaveData(); }
-
-void FileLogger::UpdateInterval(const size_t interval) {
-    config.interval = interval;
-}
-
-using namespace std::chrono_literals;
-
-void FileLogger::AddData(LogDataInterface* pLogData) {
-    // update timestamp
-    const auto currentTimeStamp = std::chrono::system_clock::now();
-
-    // update cache
-    cache.emplace_back(currentTimeStamp,
-        GetFormattedTimeStamp(currentTimeStamp),
-        pLogData->ToString());
-}
-
-bool FileLogger::SaveData() {
-    if (!bValid) { return false; }
-    if (cache.empty()) { return false; }
-
-    const auto fileName = cache.front().timeStampFormatted
-        + " ~ "
-        + cache.back().timeStampFormatted
-        + ".data";
-
-    const auto path = fs::path{ filePath } / fileName.c_str();
-
-    FILE* fp = nullptr;
-
-    const auto err = _wfopen_s(&fp, path.c_str(), L"wb");
-    if (err != 0 || fp == nullptr) { return false; }
-
-    size_t elementCount = 0u;
-    auto writeString = [&] (const std::string& str) {
-        return fwrite(str.data(), str.size(), 1, fp);
-        };
-
-    if (metaDataCb != nullptr) {
-        // write meta data
-        elementCount += writeString("MetaData: ");
-        elementCount += writeString("\r\n");
-        elementCount += writeString(*metaDataCb());
-        elementCount += writeString("\r\n");
-    }
-
-    // write dummy jump table
-    elementCount += writeString("JumpTable: ");
-    elementCount += writeString("\r\n");
-    
-    fpos_t jumpTableStart = 0;
-    if (fgetpos(fp, &jumpTableStart) != 0) { return false; }
-
-    constexpr size_t filePosition = 0;
-    for (size_t index = 0; index < cache.size(); index++) {
-        elementCount += fwrite(&filePosition, sizeof(size_t), 1, fp);
-    }
-    elementCount += writeString("\r\n");
-
-    // data region
-    elementCount += writeString("Data Region: ");
-    elementCount += writeString("\r\n");
-
-    // write cache
-    std::vector<fpos_t> jumpTable;
-
-    for (auto& it : cache) {
-        // save jump table
-        fpos_t curPos = 0;
-        if (fgetpos(fp, &curPos) != 0) { return false; }
-        jumpTable.emplace_back(curPos);
-
-        // save cache size
-        const auto cacheSize = it.timeStampFormatted.length()
-            + it.data.length()
-            + 3 * strlen("\r\n");
-        elementCount += fwrite(&cacheSize, sizeof(size_t), 1, fp);
-        elementCount += writeString("\r\n");
-
-        elementCount += writeString(it.timeStampFormatted);
-        elementCount += writeString("\r\n");
-        elementCount += writeString(it.data);
-        elementCount += writeString("\r\n");
-    }
-
-    // write jump table
-    if (fsetpos(fp, &jumpTableStart) != 0) { return false; }
-    for (auto& it : jumpTable) {
-        elementCount += fwrite(&it, sizeof(it), 1, fp);
-    }
-
-    cache.clear();
-
-    const auto ret = fclose(fp);
-
-    return ret == 0;
-}
-
-bool FileLogger::SaveDataWhenNeeded() {
-    // update timestamp
-    const auto currentTimeStamp = std::chrono::system_clock::now();
-    if (lastSaveTimeStamp == TimeStamp{}) [[unlikely]] { lastSaveTimeStamp = currentTimeStamp; }
-    const size_t interval = (currentTimeStamp - lastSaveTimeStamp) / 1ms;
-
-    // check interval
-    if (interval < config.interval) { return false; }
-    lastSaveTimeStamp = currentTimeStamp;
-
-    return SaveData();
 }
--- Src/Utilities/Logger.h
+++ Src/Utilities/Logger.h
@@ -7,34 +7,6 @@
 
 #include "StringResult.h"
 
-struct LogDataConfig {
-    // save file in binary, otherwise is human-readable
-    bool bBinary = false;
-    // compress data, only works in binary mode
-    bool bCompress = false;
-};
-
-template<>
-struct std::hash<LogDataConfig> {
-    std::size_t operator()(LogDataConfig const& s) const noexcept;
-};
-
-// Defines how data should be logged
-class LogDataInterface {  // NOLINT(cppcoreguidelines-special-member-functions)
-protected:
-    LogDataConfig config = {};
-    std::string compressed;
-
-public:
-    LogDataInterface(const LogDataConfig& conf = {}) { UpdateConfig(conf); }
-    virtual ~LogDataInterface() = default;
-
-    void UpdateConfig(const LogDataConfig& conf = {}) { this->config = conf; }
-    [[nodiscard]] const std::string& Compress(const std::string& str);
-    [[nodiscard]] virtual const std::string& ToString() = 0;
-    [[nodiscard]] virtual StringResult DataTypeInfo();
-};
-
 struct LoggerConfig {
     bool bAutoScroll = false;  // Keep scrolling if already at the bottom.
 };
@@ -53,95 +25,7 @@ struct Logger {
     void AddLog(const std::string& log);
     void AddLog(const std::string&& log);
     // with timestamp
-    void AddLog(LogDataInterface* pLogData);
+    //void AddLog(LogDataInterface* pLogData);
     void AddLog(const char* pType, const char* pLog);
     void AddLog(const std::string& type, const std::string& log);
-};
-
-constexpr auto DEFAULT_LOG_INTERVAL = 1000;
-constexpr auto FILEPATH_LENGTH = 512;
-
-struct FileLoggerConfig {
-    // interval to write to disk
-    size_t interval = DEFAULT_LOG_INTERVAL;
-    char filePath[FILEPATH_LENGTH] = "Log/";
-};
-
-template<>
-struct std::hash<FileLoggerConfig> {
-    std::size_t operator()(FileLoggerConfig const& s) const noexcept;
-};
-
-struct Ctx;
-
-// log data to the disk
-class FileLogger { // NOLINT(cppcoreguidelines-special-member-functions)
-    FileLoggerConfig config = {};
-
-    bool bValid = false;
-    std::string filePath;
-
-    using TimeStamp = decltype(std::chrono::system_clock::now());
-    TimeStamp lastSaveTimeStamp = {};
-
-    struct CacheData {
-        TimeStamp timeStamp = {};
-        std::string timeStampFormatted = {};
-        std::string data;
-    };
-
-    std::vector<CacheData> cache;
-
-public:
-    FileLogger(const FileLoggerConfig& config = {});
-    ~FileLogger();
-
-    void UpdateInterval(const size_t interval);
-    // add data to internal cache
-    void AddData(LogDataInterface* pLogData);
-
-    using MetaDataCb = std::function<const std::string* ()>;
-    MetaDataCb metaDataCb = nullptr;
-    // add metadata, which is written to the beginning of file
-    void AddMetaData(const MetaDataCb& cb) { metaDataCb = cb; }
-    // write cache to disk
-    // return true if file saved
-    bool SaveData();
-    // similiar as above, but check interval
-    bool SaveDataWhenNeeded();
-};
-
-template<typename DataInterface>
-concept ValidDataInterface = requires(DataInterface device) {
-    std::is_base_of_v<LogDataInterface, DataInterface>;
-    device.GetData();
-    device.UpdateData(std::decay_t<decltype(device.GetData())>{});
-};
-
-template<ValidDataInterface DataInterface>
-struct FileLoggerHelper {
-    FileLogger logger;
-    DataInterface dataInterface;
-
-    FileLoggerHelper(const FileLoggerConfig& loggerConf = {},
-        const LogDataConfig& logDataConf = {})
-        :logger(loggerConf), dataInterface(logDataConf) {}
-
-private:
-    using DataType = std::decay_t<decltype(dataInterface.GetData())>;
-    void UpdateData(const DataType& data) { dataInterface.UpdateData(data); }
-    void AddData() { logger.AddData(&dataInterface); }
-
-public:
-    // ------------------------
-    // serialization
-    // ------------------------
-    void AddData(const DataType& data) { UpdateData(data); AddData(); }
-    void AddMetaData(const FileLogger::MetaDataCb& cb) { logger.AddMetaData(cb); }
-
-    // ------------------------
-    // save to file
-    // ------------------------
-    bool SaveData() { return logger.SaveData(); }
-    bool SaveDataWhenNeeded() { return logger.SaveDataWhenNeeded(); }
 };
