7d60aa4dc17632ead5255aa9c379878f72bf1bec
Defisym <defisym@outlook.com>
2025-07-29 18:50:40 +0800

    add `DataInterfaceBase`

--- Src/DataInterface/DataInterface.h
+++ Src/DataInterface/DataInterface.h
@@ -19,6 +19,26 @@ struct std::hash<DataInterfaceConfig> {
 bool Compress(const std::string& in, std::string& out);
 bool UnCompress(const std::string& in, std::string& out);
 
+class DataInterfaceBase {
+protected:
+    DataInterfaceConfig config = {};
+    std::string compress;
+
+public:
+    DataInterfaceBase(const DataInterfaceConfig& conf = {}) { UpdateConfig(conf); }
+    virtual ~DataInterfaceBase() = default;
+
+    void UpdateConfig(const DataInterfaceConfig& conf = {}) { this->config = conf; }
+    [[nodiscard]] const std::string& Compress(const std::string& str) {
+        if (!config.bCompress) { return str; }
+        ::Compress(str, compress); return compress;
+    }
+    [[nodiscard]] const std::string& UnCompress(const std::string& str) {
+        if (!config.bCompress) { return str; }
+        ::UnCompress(str, compress); return compress;
+    }
+};
+
 template<typename DataStringify, typename DataType>
 concept ValidDataStringify = requires(DataStringify dataStringify) {
     { dataStringify.DataTypeInfo() } -> std::same_as<const char*>;
@@ -27,21 +47,14 @@ concept ValidDataStringify = requires(DataStringify dataStringify) {
 
 template<typename DataStringify, typename DataType>
     requires ValidDataStringify<DataStringify, DataType>
-class DataInterface {  // NOLINT(cppcoreguidelines-special-member-functions)
+class DataStringifyInterface :public DataInterfaceBase {  // NOLINT(cppcoreguidelines-special-member-functions)
 protected:
-    DataInterfaceConfig config = {};
     DataStringify stringify = {};
-    std::string compressed;
 
 public:
-    DataInterface(const DataInterfaceConfig& conf = {}) { UpdateConfig(conf); }
-    ~DataInterface() = default;
+    DataStringifyInterface(const DataInterfaceConfig& conf = {}) :DataInterfaceBase(conf) {}
+    ~DataStringifyInterface() = default;
 
-    void UpdateConfig(const DataInterfaceConfig& conf = {}) { this->config = conf; }
-    [[nodiscard]] const std::string& Compress(const std::string& str) {
-        if (!config.bCompress) { return str; }
-        ::Compress(str, compressed); return compressed;
-    }
     [[nodiscard]] const std::string& ToString(const DataType& data) { 
         return Compress(stringify.ToString(data, config.bBinary));
     }
@@ -54,7 +67,13 @@ template <typename T>
 struct is_data_interface : std::false_type {};
 
 template <typename DataStringify, typename DataType>
-struct is_data_interface<DataInterface<DataStringify, DataType>> : std::true_type {};
+struct is_data_interface<DataStringifyInterface<DataStringify, DataType>> : std::true_type {};
 
 template <typename T>
-concept ValidDataInterfaceEx = is_data_interface<T>::value;
+concept ValidDataStringifyEx = is_data_interface<T>::value;
+
+
+template<typename DataParse, typename DataType, typename ParseParam>
+concept ValidDataDataParse = requires(DataParse dataStringify) {
+    { dataStringify.ToString(DataType{}, ParseParam{}) } -> std::same_as<DataType>;
+};
