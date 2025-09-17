3b574d2affc0780e0a6479ec699f5d2bec9e69f7
Defisym <defisym@outlook.com>
2025-09-17 18:35:57 +0800

    `ComponentPerformanceBase`

---
+++ Src/GUIContent/Components/Basic/ComponentPerformanceBase.cpp
@@ -0,0 +1,29 @@
+#include "ComponentPerformanceBase.h"
+
+#include "IMGuiInterface/IMGuiContext.h"
+
+void ComponentPerformanceBase(IMGUIContext* pCtx){
+	if (!ImGui::CollapsingHeader(I18N("Performance"), ImGuiTreeNodeFlags_DefaultOpen)) {
+        return;
+    }
+
+    ImGui::Text(I18N("Application average %.3f ms/frame (%.1f FPS)"),
+		1000.0f / pCtx->pIO->Framerate, pCtx->pIO->Framerate);
+    ImGui::SameLine();
+
+    const auto ms = 1000.0f * pCtx->pIO->DeltaTime;
+    ImGui::Text(I18N("Last frame: %.3f ms"), ms);
+    ImGui::SameLine();
+
+    ImGui::Text(I18N("Main loop: %.3f ms, GUI: %.3f ms, Render: %.3f ms"),
+		pCtx->mainLoopTime, pCtx->guiTime, pCtx->renderTime);
+    ImGui::SameLine();
+
+    const auto other = ms - (pCtx->mainLoopTime + pCtx->guiTime + pCtx->renderTime);
+    if (pCtx->bVSync) {
+        ImGui::Text(I18N("V-sync wait & other process: %.3f ms"), other);
+    }
+    else {
+        ImGui::Text(I18N("Other process: %.3f ms"), other);
+    }
+}
